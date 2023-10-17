e30171662c076a4dc9120e9698f8b1d3add4976f
Aome Itinose <ky0bu@ya.ru>
2023-10-17 11:21:46 +0300

    THe 7th

--- Laba 6/3 Task.py
+++ Laba 6/3 Task.py
@@ -1,11 +1,17 @@
+import time
 from tkinter import *
 from tkinter import ttk
+from threading import *
 
-frameWidth = 300  # ширина страницы
-frameHeigth = 300  # высота страницы
-ovalRadius = 30  # радиус кругов
+frameWidth = 120  # ширина страницы
+frameHeigth = 340  # высота страницы
+ovalRadius = 50  # радиус кругов
 c = 10  # растояние кругов от стенки
 diff = 5  # растояние между шарами
+
+redTime = 5
+yellowTime = 2
+greenTime = 5
 
 frame = Tk()
 canvas = Canvas(frame, width=frameWidth, height=frameHeigth)
@@ -15,15 +21,18 @@ yellow = canvas.create_oval(c, diff + c + 2 * ovalRadius, c + 2 * ovalRadius, di
                             outline="black", width=2)
 green = canvas.create_oval(c, 2 * diff + c + 4 * ovalRadius, c + 2 * ovalRadius, 2 * diff + c + 6 * ovalRadius,
                            fill="white", outline="black", width=2)
-
+greenTimer = Label(background="white", font="Arial 25 normal roman")
+redTimer = Label(background="white", font="Arial 25 normal roman")
 
 def setRed():
     canvas.itemconfig(red, fill="red")
     canvas.itemconfig(yellow, fill="white")
     canvas.itemconfig(green, fill="white")
+    greenTimer.config(background="white")
+    greenTimer.config(text="")
+    redTimer.config(background="red")
 
 def setYellow():
-    canvas.itemconfig(red, fill="white")
     canvas.itemconfig(yellow, fill="yellow")
     canvas.itemconfig(green, fill="white")
 
@@ -31,16 +40,33 @@ def setGreen():
     canvas.itemconfig(red, fill="white")
     canvas.itemconfig(yellow, fill="white")
     canvas.itemconfig(green, fill="green")
+    greenTimer.config(background="green")
+    redTimer.config(background="white")
+    redTimer.config(text="")
 
+cnt = 5
+def color():
+    while True:
+        setRed()
+        for i in range(redTime-yellowTime):
+            redTimer.config(text=str(redTime-i))
+            time.sleep(1)
 
-redButton = Button(text="Set red.", command=setRed)
-yellowButton = Button(text="Set yellow.", command=setYellow)
-greenButton = Button(text="Set green.", command=setGreen)
+        setYellow()
+        for i in range(yellowTime):
+            redTimer.config(text=str(yellowTime-i))
+            time.sleep(1)
 
+        setGreen()
+        for i in range(greenTime):
+            greenTimer["text"] = str(greenTime - i)
+            greenTimer.update()
+            time.sleep(1)
+
+thr = Thread(target=color, args=())
+thr.start()
+
+redTimer.place(x=48, y=40)
+greenTimer.place(x=48, y=250)
 canvas.pack()
-
-redButton.pack()
-yellowButton.pack()
-greenButton.pack()
-
 frame.mainloop()
